d4d0f79995d02d2b447ac42eac613ee9ab5b2357
Filippo <filippo@ubuntu.com>
2023-10-21 19:06:38 +0200

    Added part of centers for next-event simulation

--- src/main/java/it/pmcsn/controllers/NextEventController.java
+++ src/main/java/it/pmcsn/controllers/NextEventController.java
@@ -1,14 +1,29 @@
 package it.pmcsn.controllers;
 
+import it.pmcsn.centers.AbstractCenter;
 import it.pmcsn.event.Event;
 import it.pmcsn.rngs.Rngs;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NextEventController {
 
     public Rngs rngs;
-    public List<Event> eventList;
+    public List<Event> eventList = new ArrayList<>();
+    public List<AbstractCenter> centerList = new ArrayList<>();
+
+    private int STOP_TIME; //"Gate closed" time
+
+    private int totalArrivals = 0;
+
+    private boolean isGateOpen = true;
+
+
+
+
+
+
 
 
 
